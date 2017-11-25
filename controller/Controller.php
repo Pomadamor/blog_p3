8d5421570748b920f39b503b55c8e2d5d5577ea9
Lerna Ines <eck.ines@gmail.com>
2017-11-25 15:55:28 +0100

    MAJ autorisation utilisateur

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,20 +1,15 @@
 <?php
-
-// require("model/Article.php");
-// require("model/DatabaseConnexion.php");
-// require("model/ArticleRepository.php");
-
-
 class Controller{
+
   public function homeController(){
     ob_start();
-    $articleRepo=new ArticleRepository();
-    $articles=$articleRepo->findAll('Article');
+    $articleRepo = new ArticleRepository();
+    $articles    = $articleRepo->findAll('Article');
     foreach ($articles as $article) {
       $article->fetchComments();
     }
-    $userRepo = new UserRepository();
-    $user = $userRepo->getLoggedUser();
+    $userRepo    = new UserRepository();
+    $user        = $userRepo->getLoggedUser();
     if($user === false){
       include('view/header.php');
       include('view/home.php');
@@ -28,14 +23,14 @@ class Controller{
       include('view/homeConnecte.php');
       include('view/footerAdmin.php');
     }
-    $html= ob_end_flush();
+    $html = ob_end_flush();
     return $html;
   }
 
     public function mentionController(){
       ob_start();
       $userRepo = new UserRepository();
-      $user = $userRepo->getLoggedUser();
+      $user     = $userRepo->getLoggedUser();
       if($user === false){
         include('view/header.php');
         include('view/mentionlegale.php');
@@ -49,17 +44,17 @@ class Controller{
         include('view/mentionlegale.php');
         include('view/footerAdmin.php');
       }
-      $html= ob_end_flush();
+      $html = ob_end_flush();
       return $html;
     }
 
   public function articleController($id){
     ob_start();
-    $articleRepo=new ArticleRepository();
-    $articles=$articleRepo->find($id, 'Article');
+    $articleRepo = new ArticleRepository();
+    $articles    = $articleRepo->find($id, 'Article');
     $articles->fetchCommentsAll();
-    $userRepo = new UserRepository();
-    $user = $userRepo->getLoggedUser();
+    $userRepo    = new UserRepository();
+    $user        = $userRepo->getLoggedUser();
     if($user === false){
       include('view/header.php');
       include('view/article.php');
@@ -73,61 +68,66 @@ class Controller{
       include('view/articleConnecte.php');
       include('view/footerAdmin.php');
     }
-    $html= ob_end_flush();
+    $html = ob_end_flush();
     return $html;
   }
 
   public function connexionController(){
       ob_start();
       include('view/connexion.php');
-      $html= ob_end_flush();
+      $html = ob_end_flush();
       return $html;
   }
 
   public function inscriptionController(){
       ob_start();
       include('view/inscription.php');
-      $html= ob_end_flush();
+      $html = ob_end_flush();
       return $html;
   }
 
   public function commentController(){
       ob_start();
-      $userRepo = new UserRepository();
-      $user = $userRepo->getLoggedUser();
-      $commentRepo=new CommentRepository();
-      $comments=$commentRepo->findAllById($user);
-
-      include('view/headerConnecte.php');
-      include('view/commentConnecte.php');
-      include('view/footerAdmin.php');
-      $html= ob_end_flush();
+      $userRepo    = new UserRepository();
+      $user        = $userRepo->getLoggedUser();
+      $commentRepo = new CommentRepository();
+      $comments    = $commentRepo->findAllById($user);
+      if( ($user !== false) && ($user->getAdmin() == True) ){
+        include('view/headerConnecte.php');
+        include('view/commentConnecte.php');
+        include('view/footerAdmin.php');
+      }
+      $html = ob_end_flush();
       return $html;
   }
 
   public function articleAdmin(){
     ob_start();
-    $articleRepo=new ArticleRepository();
-    $articles=$articleRepo->findAll('Article');
+    $articleRepo = new ArticleRepository();
+    $articles    = $articleRepo->findAll('Article');
+    $userRepo    = new UserRepository();
+    $user        = $userRepo->getLoggedUser();
     foreach ($articles as $article) {
       $article->fetchComments();
     }
-    if(!empty($_SESSION)){
+    if($user !== false){
       include('view/headerAdmin.php');
       include('view/articleAdmin.php');
       include('view/footerAdmin.php');
     }else{
       echo "vous n'êtes pas connecter";
     }
-    $html= ob_end_flush();
+    $html = ob_end_flush();
     return $html;
   }
 
   public function commentAdmin(){
     ob_start();
-    $commentRepo=new CommentRepository();
-    $comments=$commentRepo->findAllBySignale();
-    if(!empty($_SESSION)){
+    $commentRepo = new CommentRepository();
+    $comments    = $commentRepo->findAllBySignale();
+    $userRepo    = new UserRepository();
+    $user        = $userRepo->getLoggedUser();
+    if( ($user !== false) && ($user->getAdmin() == True) ){
       include('view/headerAdmin.php');
       include('view/commentAdmin.php');
       include('view/footerAdmin.php');
@@ -140,7 +140,9 @@ class Controller{
 
   public function articleAdd(){
       ob_start();
-      if(!empty($_SESSION)){
+      $userRepo = new UserRepository();
+      $user     = $userRepo->getLoggedUser();
+      if( ($user !== false) && ($user->getAdmin() == True) ){
         include('view/headerAdmin.php');
         include('view/articleAdd.php');
         include('view/footerAdmin.php');
@@ -153,7 +155,9 @@ class Controller{
 
   public function modifierArticle(){
       ob_start();
-      if(!empty($_SESSION)){
+      $userRepo = new UserRepository();
+      $user     = $userRepo->getLoggedUser();
+      if( ($user !== false) && ($user->getAdmin() == True) ){
         $id=$_POST['id'];
         $articleRepo=new ArticleRepository();
         $articles=$articleRepo->find($id, 'Article');
